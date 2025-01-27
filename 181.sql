19d6dfaec8fb30faf9ac516ef623e6cb125721ae
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-27 16:27:55 +0530

    Update 181.sql

--- 181.sql
+++ 181.sql
@@ -3,5 +3,8 @@ select e.name as employee from employee e
 join employee m on e.managerID = m.id
 where e.salary > m.salary
 
- here we have to find the the employee who are earning more than there managers.
-In this code we have 
+Here we have to find the employees who are earning more than their managers.
+Starting of with the code we are going to select all the variables which we need 
+Which is going to be the employee id and manager ID.
+So then we are going to join it on employee id and manager id
+Finally owuld be the command statement which will be 
