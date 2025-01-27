94511aedc15d9856c980b5920c95ca7173eb040f
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-27 21:22:01 +0530

    answer 182.sql

---
+++ 182.sql
@@ -0,0 +1,6 @@
+select email from person
+group by email having count(email)>1;
+--
+in this we have to find the duplicate emails in the given table and diplay it
+so i have used a different appraoch rather than the straight forward one where i have 
+deleted the emails which have once been having count more than one
